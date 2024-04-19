0c839ff144fdc942952eeb2176ed80737f25beaf
PentiukPavel <pentukpavel@yandex.ru>
2024-04-19 15:37:15 +0300

    start django project

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # sunny_backend
 Бекэнд для проекта "Солнечный Екатеринбург"
+[![road_repair workflow](https://github.com/SunnyEkb/sunny_backend/actions/workflows/sunny_ekb.yaml/badge.svg)](https://github.com/SunnyEkb/sunny_backend/actions/workflows/sunny_ekb.yaml)
+
+## Описание
+Web приложение для улучшения качества жизни резидентов района "Солнечный" города Екатеринбург
+
+## Для разработчиков:
+
+### Приложения:
+- _config_: общие настройки проекта;
+
+### Пример файла с переменными среды:
+".env.example" в корневой папке проекта:
+
+### Линтер:
+`black`.
+
+### Pre-commit:
+Настроен pre-commit для проверки оформления кода.
+Для проверки кода перед выполнением операции commit, выполнить команду:
+
+```
+pre-commit run --all-files
+```
+
+## Как запустить проект:
+
+Клонировать проект
+```
+git clone https://github.com:SunnyEkb/sunny_backend.git
+```
+
+Переименовать файл .env.example и изменить содержимое на актуальные данные.
+```
+mv .env.example .env
+```
+
+### 1) Запуск проекта на локальной машине:
+
+В корневой папка создать виртуальное окружение:
+```
+py -3 -m venv venv
+```
+
+Активировать виртуальное окружение:
+```
+source venv/Scripts/activate
+```
+
+Установить зависимости из файла requirements.txt:
+```
+pip install -r requirements.txt
+```
+
+В папке с файлом manage.py (./src) выполнить следующие команды для миграций:
+```
+python manage.py migrate
+```
+
+Создать суперюзера:
+```
+python manage.py createsuperuser
+```
+
+Запустить проект:
+```
+python manage.py runserver
+```
+
+### 2) Запуск через Docker:
+Запустить контейнер c проектом
+```
+docker-compose up -d
+```
+
+Выполнить миграции:
+```
+docker-compose exec backend python manage.py migrate
+```
+
+Проект будет доступен на 8000 порту.
+Swager доступен по адресу:
+```
+http://localhost:8000/api/v1/docs/
+```
+
+Если отсутствуют статические файлы, то выполнить
+```
+docker-compose exec backend python manage.py collectstatic --no-input
+```
+
+## Системные требования
+### Python==3.12
+
+## Стек
+### Django
+### Django REST Framework
+### PosrgreSQL
---
+++ src/config/settings.py
@@ -0,0 +1,127 @@
+import os
+import sys
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+TRUE_VALUES = ["1", "true", "True", "YES", "yes"]
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_path = os.path.join(os.path.dirname(__file__), "../../.env")
+load_dotenv(dotenv_path)
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = True if os.getenv("DEBUG") in TRUE_VALUES else False
+
+if "test" in sys.argv:
+    ALLOWED_HOSTS = ["testserver"]
+else:
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="*").split(", ")
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [TEMPLATES_DIR],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+PROD_DB = os.getenv("PROD_DB") in TRUE_VALUES
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    }
+elif PROD_DB and "test" not in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("POSTGRES_USER"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "sunny.sqlite3",
+        }
+    }
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": (
+            "django.contrib.auth.password_validation"
+            ".UserAttributeSimilarityValidator"
+        ),
+    },
+    {
+        "NAME": (
+            "django.contrib.auth.password_validation.MinimumLengthValidator"
+        ),
+    },
+    {
+        "NAME": (
+            "django.contrib.auth.password_validation.CommonPasswordValidator"
+        ),
+    },
+    {
+        "NAME": (
+            "django.contrib.auth.password_validation.NumericPasswordValidator"
+        ),
+    },
+]
+
+LANGUAGE_CODE = "ru-ru"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
