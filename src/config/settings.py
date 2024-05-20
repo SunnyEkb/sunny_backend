b2f1db2247e3fd945045a6e378f17fecd4f4732d
PentiukPavel <pentukpavel@yandex.ru>
2024-05-20 16:02:57 +0300

    update user

--- src/config/settings.py
+++ src/config/settings.py
@@ -45,12 +45,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -184,10 +184,9 @@ SESSION_COOKIE_SAMESITE = "Lax"
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "CSRF_TRUSTED_ORIGINS", "http://127.0.0.1"
 ).split(", ")
-# CORS_ALLOWED_ORIGINS = os.getenv(
-#    "CORS_ALLOWED_ORIGINS", "http://127.0.0.1"
-# ).split(", ")
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = os.getenv(
+   "CORS_ALLOWED_ORIGINS", "http://127.0.0.1"
+).split(", ")
 CORS_ALLOW_METHODS = (
     "DELETE",
     "GET",
