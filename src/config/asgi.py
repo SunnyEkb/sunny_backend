ace13e7d3d2a96d4bb0ca3085b60a245c9a20e6a
PentiukPavel <pentukpavel@yandex.ru>
2024-06-04 15:26:03 +0300

    notifications

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,7 +1,11 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from notifications.routing import websockets_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
@@ -10,5 +14,10 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(websockets_urlpatterns)
+            )
+        )
     }
 )
--- src/notifications/consumers.py
+++ src/notifications/consumers.py
@@ -2,23 +2,14 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
 class NotificationConsumer(AsyncJsonWebsocketConsumer):
-
     async def connect(self) -> None:
-        """On socket connection. Allowed only for authorized users."""
         await self.accept()
-        if self.scope['user'] is None:
+        if self.scope["user"] is None:
             await self.close()
             return
-        self.user = self.scope['user']
-        await self.channel_layer.group_add(
-            self.account_id,
-            self.channel_name
-        )
+        self.user = self.scope["user"]
+        await self.channel_layer.group_add(self.user.id, self.channel_name)
 
     async def disconnect(self, code) -> None:
-        """On disconnect socket."""
-        await self.channel_layer.group_discard(
-            self.account_id,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.user.id, self.channel_name)
         await self.close(code)
