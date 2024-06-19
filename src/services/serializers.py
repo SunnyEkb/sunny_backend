92a21627d16b3f22ce607b352b60c9bfe7ccfbe6
PentiukPavel <pentukpavel@yandex.ru>
2024-06-19 15:18:47 +0300

    feat: add services serializers

--- src/services/serializers.py
+++ src/services/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from services.models import Type
+from services.models import Service, ServiceImage, Type
 
 
 class TypeGetSerializer(serializers.ModelSerializer):
@@ -8,4 +8,59 @@ class TypeGetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Type
-        fields = ("category", "title")
+        fields = ("id", "category", "title")
+
+
+class ServiceImageSerializer(serializers.ModelSerializer):
+    """Сериализатор для получения фото услуг."""
+
+    class Meta:
+        model = ServiceImage
+        fields = ("image",)
+
+
+class ServiceCreateUpdateSerializer(serializers.ModelSerializer):
+    """Сериализатор для создания и изменения услуги."""
+
+    images = ServiceImageSerializer(many=True, required=False)
+    type = serializers.StringRelatedField()
+
+    class Meta:
+        model = Service
+        fields = (
+            "title",
+            "description",
+            "experience",
+            "place_of_provision",
+            "type",
+            "price",
+        )
+
+    def create(self, validated_data):
+        images = validated_data.pop("images")
+        service = Service.objects.create(**validated_data)
+        for image in images:
+            ServiceImage.objects.create(service=service, image=image)
+        return service
+
+
+class ServiceRetreiveSerializer(serializers.ModelSerializer):
+    """Сериализатор для просмотра услуги."""
+
+    provider = serializers.StringRelatedField(read_only=True)
+    images = ServiceImageSerializer(many=True, read_only=True, source="images")
+    type = TypeGetSerializer()
+
+    class Meta:
+        model = Service
+        fields = (
+            "id",
+            "provider",
+            "title",
+            "description",
+            "experience",
+            "place_of_provision",
+            "type",
+            "price",
+            "status",
+        )
