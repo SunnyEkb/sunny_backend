7201d1d9e4f887021e4d0a25205854f6fcc23574
PentiukPavel <pentukpavel@yandex.ru>
2024-06-20 12:06:44 +0300

    feat: add services filter tests

--- src/api/v1/services/tests/test_views.py
+++ src/api/v1/services/tests/test_views.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from django.urls import reverse
 
-from core.choices import ServiceStatus
+from core.choices import ServicePlace, ServiceStatus
 from core.fixtures import TestServiceFixtures
 from services.models import Service, Type
 
@@ -55,3 +55,65 @@ class TestServivecesView(TestServiceFixtures):
             len(response_anon_user.json()),
             len(Service.objects.filter(status=ServiceStatus.PUBLISHED.value)),
         )
+
+    def test_services_filters(self):
+        templates = {
+            "category": [
+                self.type_1.category,
+                (
+                    Service.objects.filter(
+                        status=ServiceStatus.PUBLISHED.value
+                    ).filter(type__category=self.type_1.category)
+                ),
+            ],
+            "type": [
+                self.type_1.title,
+                (
+                    Service.objects.filter(
+                        status=ServiceStatus.PUBLISHED.value
+                    ).filter(type__title__icontains=self.type_1.title)
+                ),
+            ],
+            "title": [
+                self.service_1.title,
+                (
+                    Service.objects.filter(
+                        status=ServiceStatus.PUBLISHED.value
+                    ).filter(title__icontains=self.service_1.title)
+                ),
+            ],
+            "experience_max": [
+                1,
+                (
+                    Service.objects.filter(
+                        status=ServiceStatus.PUBLISHED.value
+                    ).filter(experience__lte=1)
+                ),
+            ],
+            "experience_min": [
+                10,
+                (
+                    Service.objects.filter(
+                        status=ServiceStatus.PUBLISHED.value
+                    ).filter(experience__gte=10)
+                ),
+            ],
+            "place_of_provision": [
+                ServicePlace.OPTIONS.value,
+                (
+                    Service.objects.filter(
+                        status=ServiceStatus.PUBLISHED.value
+                    ).filter(place_of_provision=ServicePlace.OPTIONS.value)
+                ),
+            ],
+            "my_services": [
+                True,
+                Service.objects.filter(provider=self.user_1),
+            ],
+        }
+        for k, v in templates.items():
+            with self.subTest(filter=k):
+                response = self.client_1.get(
+                    reverse("services-list") + f"?{k}={v[0]}"
+                )
+                self.assertEqual(len(response.data), len(v[1]))
