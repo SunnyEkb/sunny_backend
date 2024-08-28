2a3baa5133c72ff84d5dbcd57c22c9eb65c44a3e
PentiukPavel <pentukpavel@yandex.ru>
2024-08-28 10:30:55 +0300

    ref: change project structure - ads views

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -2,9 +2,15 @@ from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-from api.v1.views.favorite_views import FavoritesViewSet
+from api.v1.views import (
+    AdViewSet,
+    CategoryViewSet,
+    FavoritesViewSet,
+)
 
 api_v1_router = DefaultRouter()
+api_v1_router.register("ads", AdViewSet, basename="ads")
+api_v1_router.register("categories", CategoryViewSet, basename="categories")
 api_v1_router.register("favorite", FavoritesViewSet, "favorite")
 
 urlpatterns = [
@@ -17,7 +23,6 @@ urlpatterns = [
     ),
     path("", include("api.v1.users.urls")),
     path("", include("api.v1.services.urls")),
-    path("", include("api.v1.ads.urls")),
     path("", include("api.v1.comments.urls")),
     path("", include(api_v1_router.urls)),
 ]
---
+++ src/api/v1/views/__init__.py
@@ -0,0 +1,2 @@
+from api.v1.views.ads_views import *
+from api.v1.views.favorite_views import *
