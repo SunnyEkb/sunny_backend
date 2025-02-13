7c051e674b77bf90590993e59d04a649a532ff34
PentiukPavel <pentukpavel@yandex.ru>
2025-02-13 09:39:07 +0300

    feat: create permission class for moderator

--- src/api/v1/permissions.py
+++ src/api/v1/permissions.py
@@ -88,3 +88,11 @@ class CommentAuthorOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
+
+
+class ModeratorOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_moderator
