ee4d33e5cdd80c11aba03044e52cbd27a9ef7859
PentiukPavel <pentukpavel@yandex.ru>
2025-05-23 15:31:52 +0300

    ref: reformat task of moderation comment

--- src/bad_word_filter/tasks.py
+++ src/bad_word_filter/tasks.py
@@ -1,6 +1,7 @@
 from bad_word_filter.bad_word_filter import bad_words_filter
 
 from celery import shared_task
+from rest_framework.request import Request
 
 from comments.models import Comment
 from core.choices import SystemMessages
@@ -9,15 +10,17 @@ from notifications.models import Notification
 
 
 @shared_task
-def moderate_comment(comment_id: int, request: str) -> None:
+def moderate_comment_task(comment_id: int, request: Request) -> None:
     """
-    Задача по модерации комментария.
+    Постановка задача по модерации комментария.
 
     :param comment_id: идентификатор комментария
-    :param request: экземпляр запроса
+    :param request: экземпляр объекта запроса
     """
-    comment: Comment = Comment.objects.filter(pk=comment_id).first()
-    if comment:
+
+    comment = Comment.objects.filter(pk=comment_id)
+    if comment.exists():
+        comment: Comment = comment.first()
         if len(bad_words_filter(comment.feedback)) > 0:
             Notification.objects.create(
                 receiver=comment.author,
