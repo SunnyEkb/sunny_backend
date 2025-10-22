e6dd2fca65794c3771bf11e4f7bf138b761ab582
PentiukPavel <pentukpavel@yandex.ru>
2025-10-22 16:03:38 +0300

    ref: disable automatic comment moderation

--- src/bad_word_filter/tasks.py
+++ src/bad_word_filter/tasks.py
@@ -1,11 +1,13 @@
-from bad_word_filter.bad_word_filter import bad_words_filter
+# from bad_word_filter.bad_word_filter import bad_words_filter
 
 from celery import shared_task
 
 from comments.models import Comment
-from core.choices import SystemMessages
+
+# from core.choices import SystemMessages
 from core.utils import notify_about_moderation
-from notifications.models import Notification
+
+# from notifications.models import Notification
 
 
 @shared_task
@@ -20,10 +22,13 @@ def moderate_comment_task(comment_id: int) -> None:
     comments = Comment.objects.filter(pk=comment_id)
     if comments.exists():
         comment: Comment = comments.first()
-        if len(bad_words_filter(comment.feedback)) > 0:
-            Notification.objects.create(
-                receiver=comment.author,
-                text=SystemMessages.AUTOMATIC_COMMENT_MODERATION_FAILED,
-            )
-        else:
-            notify_about_moderation(comment.get_admin_url())
+        notify_about_moderation(comment.get_admin_url())
+
+
+#        if len(bad_words_filter(comment.feedback)) > 0:
+#            Notification.objects.create(
+#                receiver=comment.author,
+#                text=SystemMessages.AUTOMATIC_COMMENT_MODERATION_FAILED,
+#            )
+#        else:
+#            notify_about_moderation(comment.get_admin_url())
