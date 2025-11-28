94418e46c0f5005d192da840d5c7dd5032f59c68
PentiukPavel <pentukpavel@yandex.ru>
2025-11-28 14:11:30 +0300

    feat: create serializers for categories

---
+++ src/api/v1/serializers/categories_serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from categories.models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор для получения списка категорий."""
+
+    subcategories = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ("id", "title", "image", "subcategories")
+
+    def get_subcategories(self, obj):
+        if obj.subcategories.exists():
+            subcat = []
+            for subcategory in obj.subcategories.all():
+                subcat.append(CategorySerializer(subcategory).data)
+            return subcat
+        else:
+            return None
