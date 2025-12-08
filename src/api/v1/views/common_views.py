4c89ec61d60dce2882933468b8685c2c5dbc083c
PentiukPavel <pentukpavel@yandex.ru>
2025-12-08 13:06:00 +0300

    feat: add method to treat queryset

--- src/api/v1/views/common_views.py
+++ src/api/v1/views/common_views.py
@@ -1,10 +1,24 @@
 from rest_framework.views import APIView
 
+from api.v1.validators import validate_id
+from core.choices import AdvertisementStatus
+
 
 class AdvertisementView(APIView):
     """Класс для получения объявлений всех типов."""
 
-    def list(self):
+    def list(self, request):
         """Получение списка объявлений."""
 
         pass
+
+    def treat_queryset(self, queryset):
+        if self.action == "list":
+            params = self.request.query_params
+            if "category_id" in params:
+                category_id = params.get("category_id")
+                validate_id(category_id)
+                queryset = queryset.filter(
+                    category__id=category_id,
+                    status=AdvertisementStatus.PUBLISHED,
+                )
