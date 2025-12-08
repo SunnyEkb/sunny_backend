b3cc6d57249ad1e2ffffac34734d3fab60ed949b
PentiukPavel <pentukpavel@yandex.ru>
2025-12-08 10:51:31 +0300

    feat: create view template

---
+++ src/api/v1/views/common_views.py
@@ -0,0 +1,10 @@
+from rest_framework.views import APIView
+
+
+class AdvertisementView(APIView):
+    """Класс для получения объявлений всех типов."""
+
+    def list(self):
+        """Получение списка объявлений."""
+
+        pass
