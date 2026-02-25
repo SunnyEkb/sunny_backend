4279eabafae4be9fc8501038cfdd91904071b5f0
PentiukPavel <pentukpavel@yandex.ru>
2026-02-25 17:48:25 +0300

    ref: correct codestyle for ads

--- src/ads/documents.py
+++ src/ads/documents.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Model, QuerySet
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
@@ -35,7 +36,10 @@ class AdDocument(Document):
         """Настройки ElasticSearch."""
 
         name = "ads"
-        settings = {"number_of_shards": 1, "number_of_replicas": 0}  # noqa: RUF012
+        settings = {  # noqa: RUF012
+            "number_of_shards": 1,
+            "number_of_replicas": 0,
+        }
 
     class Django:
         """Настройки Django."""
@@ -51,9 +55,25 @@ class AdDocument(Document):
 
         related_models = [User]  # noqa: RUF012
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
+        """Получить запрос к БД.
+
+        Returns:
+            QuerySet: запрос к БД
+
+        """
         return super().get_queryset().select_related("provider")
 
-    def get_instances_from_related(self, related_instance):
+    def get_instances_from_related(self, related_instance: Model) -> list[Ad] | None:
+        """Получить список объявлений пользователя.
+
+        Args:
+            related_instance (Model): связаная модель
+
+        Returns:
+            list[Ad] | None: список объявлений пользователя
+
+        """
         if isinstance(related_instance, User):
             return related_instance.ads.all()
+        return None
