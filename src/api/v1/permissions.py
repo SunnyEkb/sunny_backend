982f8015ad95369f96a4e914d5bdeeb15893611b
PentiukPavel <pentukpavel@yandex.ru>
2026-05-14 11:17:36 +0300

    ref: correct codestyle

--- src/api/v1/permissions.py
+++ src/api/v1/permissions.py
@@ -1,37 +1,68 @@
+from typing import TYPE_CHECKING, Union
+
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 from ads.models import AdImage
 from core.choices import AdvertisementStatus
 from services.models import ServiceImage
 
+if TYPE_CHECKING:
+    from django.db.models import Model
+    from django.http import HttpRequest
+    from django.views.generic import View
+
+    from ads.models import Ad
+    from comments.models import Comment
+    from notifications.models import Notification
+    from services.models import Service
+    from users.models import CustomUser
+
 
 class SelfOnly(BasePermission):
     """Редактирование данных только о себе."""
 
-    def has_permission(self, request, view):
+    def has_permission(
+        self, request: "HttpRequest", view: "View"  # noqa: ARG002
+    ) -> bool:
         return request.user.is_authenticated
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(
+        self, request: "HttpRequest", view: "View", obj: "CustomUser"  # noqa: ARG002
+    ) -> bool:
         return obj == request.user
 
 
 class OwnerOrReadOnly(BasePermission):
     """Редактирование только своих услуг и объявлений."""
 
-    def has_permission(self, request, view):
+    def has_permission(
+        self, request: "HttpRequest", view: "View"  # noqa: ARG002
+    ) -> bool:
         return request.method in SAFE_METHODS or request.user.is_authenticated
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(
+        self,
+        request: "HttpRequest",
+        view: "View",  # noqa: ARG002
+        obj: Union["Ad", "Service"],
+    ) -> bool:
         return obj.provider == request.user
 
 
 class PhotoOwnerOrReadOnly(BasePermission):
     """Редактирование только фото своих услуг."""
 
-    def has_permission(self, request, view):
+    def has_permission(
+        self, request: "HttpRequest", view: "View"  # noqa: ARG002
+    ) -> bool:
         return request.method in SAFE_METHODS or request.user.is_authenticated
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(
+        self,
+        request: "HttpRequest",
+        view: "View",  # noqa: ARG002
+        obj: AdImage | ServiceImage,
+    ) -> bool:
         if isinstance(obj, ServiceImage):
             return obj.service.provider == request.user
         if isinstance(obj, AdImage):
@@ -40,14 +71,19 @@ class PhotoOwnerOrReadOnly(BasePermission):
 
 
 class PhotoReadOnly(BasePermission):
-    """Разрешение на просмотр фото услуги или объявления
-    незарегистрированному пользователю.
-    """
+    """Разрешение на просмотр фото незарегистрированному пользователю."""
 
-    def has_permission(self, request, view):
+    def has_permission(
+        self, request: "HttpRequest", view: "View"  # noqa: ARG002
+    ) -> bool:
         return request.method in SAFE_METHODS
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(
+        self,
+        request: "HttpRequest",  # noqa: ARG002
+        view: "View",  # noqa: ARG002
+        obj: AdImage | ServiceImage,
+    ) -> bool:
         if isinstance(obj, ServiceImage):
             return obj.service.status == AdvertisementStatus.PUBLISHED
         if isinstance(obj, AdImage):
@@ -56,14 +92,19 @@ class PhotoReadOnly(BasePermission):
 
 
 class ReadOnly(BasePermission):
-    """Разрешение на просмотр услуги или объявления
-    незарегистрированному пользователю.
-    """
+    """Разрешение на просмотр объявления незарегистрированному пользователю."""
 
-    def has_permission(self, request, view):
+    def has_permission(
+        self, request: "HttpRequest", view: "View"  # noqa: ARG002
+    ) -> bool:
         return request.method in SAFE_METHODS
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(
+        self,
+        request: "HttpRequest",
+        view: "View",  # noqa: ARG002
+        obj: Union["Ad", "Service"],
+    ) -> bool:
         return (
             obj.provider == request.user or obj.status == AdvertisementStatus.PUBLISHED
         )
@@ -72,28 +113,40 @@ class ReadOnly(BasePermission):
 class CommentAuthorOnly(BasePermission):
     """Разрешения на удаление комментария автору."""
 
-    def has_permission(self, request, view):
+    def has_permission(
+        self, request: "HttpRequest", view: "View"  # noqa: ARG002
+    ) -> bool:
         return request.user.is_authenticated
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(
+        self, request: "HttpRequest", view: "View", obj: "Comment"  # noqa: ARG002
+    ) -> bool:
         return obj.author == request.user
 
 
 class ModeratorOnly(BasePermission):
     """Разрешения на совершение действий только модератору."""
 
-    def has_permission(self, request, view):
+    def has_permission(
+        self, request: "HttpRequest", view: "View"  # noqa: ARG002
+    ) -> bool:
         return request.user.is_authenticated and request.user.is_moderator
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(
+        self, request: "HttpRequest", view: "View", obj: "Model"  # noqa: ARG002
+    ) -> bool:
         return request.user.is_moderator
 
 
 class NotificationRecieverOnly(BasePermission):
     """Только получатель уведомления."""
 
-    def has_permission(self, request, view):
+    def has_permission(
+        self, request: "HttpRequest", view: "View"  # noqa: ARG002
+    ) -> bool:
         return request.user.is_authenticated
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(
+        self, request: "HttpRequest", view: "View", obj: "Notification"  # noqa: ARG002
+    ) -> bool:
         return obj.receiver == request.user
